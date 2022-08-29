0e7cb489c1ced29ad6a910413600c59df18f8f9e
rzv <pcwebschools@gmail.com>
2022-08-29 20:06:34 +0300

    add another change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # this line will import the "website" package we just created
 # add something new
+# new change from main
 from website import create_app
 
 app = create_app()  # create instance of the application
