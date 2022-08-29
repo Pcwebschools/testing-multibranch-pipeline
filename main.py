88986c103a9b4ba0844b76e547719706ecccfa3d
rzv <pcwebschools@gmail.com>
2022-08-29 19:30:28 +0300

    Testing git inside VSCode

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # this line will import the "website" package we just created
 # add something new
+# testing git within VScode
 from website import create_app
 
 app = create_app()  # create instance of the application
