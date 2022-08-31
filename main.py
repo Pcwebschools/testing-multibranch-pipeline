dafd5084704d92b8658a8aea3eec7a0dde89e00c
rzv <pcwebschools@gmail.com>
2022-09-01 00:03:41 +0300

    testing TAG_NAME

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # this line will import the "website" package we just created
 # add something new
 # testing git within VScode
+# some update from masin branch
 from website import create_app
 
 app = create_app()  # create instance of the application
